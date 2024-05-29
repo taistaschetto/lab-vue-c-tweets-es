b6ecaf0afd1d6a4cac724d64f7b6906fa2928ce6
Tais Taschetto <taistaschetto@hotmail.com>
2024-05-29 12:17:22 +0200

    Iteration 1, 2 3 done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <Tweet />
+    <Tweet :tweet="tweets[0]" />
   </div>
 </template>
 
--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,38 +1,37 @@
+<script setup>
+const props = defineProps({
+  tweet: Object,
+});
+</script>
+
 <template>
   <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
-
+    <!-- Dynamic profile image -->
+    <img :src="tweet.user.image" class="profile" alt="profile" />
+  
     <div class="body">
       <div class="top">
         <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
+          <span class="name">{{ tweet.user.name }}</span>
+          <span class="handle">@{{ tweet.user.handle }}</span>
         </span>
-
-        <span class="timestamp">Nov 30, 2020</span>
+  
+        <span class="timestamp">{{ tweet.timestamp }}</span>
       </div>
-
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
+  
+      <p class="message">{{ tweet.message }}</p>
+  
       <div class="actions">
-        <!-- Font Awesome icons -->
         <i class="far fa-comment"></i>
         <i class="fas fa-retweet"></i>
         <i class="far fa-heart"></i>
         <i class="fas fa-share"></i>
       </div>
     </div>
-
+  
     <i class="fas fa-ellipsis-h"></i>
   </div>
-</template>
+  </template>
 
 <style scoped>
 a {
